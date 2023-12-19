616461b359c5d5162421da4a2418b3b30cfc1f12
ramonpoli <ramon.polidura@gmail.com>
2023-12-19 17:33:03 +0000

    Finish styling

---
+++ components/Buttons/PrimaryButton/PrimaryButton.tsx
@@ -0,0 +1,27 @@
+import { Box, Button, ButtonProps, Typography } from "@mui/material";
+import { FC, PropsWithChildren } from "react";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
+type Props = {
+  //
+};
+const PrimaryButton: FC<PropsWithChildren<Props & ButtonProps>> = ({
+  children,
+  ...buttonProps
+}) => {
+  return (
+    <Button
+      variant="contained"
+      size="large"
+      endIcon={
+        <Typography lineHeight={"100%"} pl={4}>
+          <ArrowForwardIosIcon fontSize={"small"} />
+        </Typography>
+      }
+      {...buttonProps}
+    >
+      {children}
+    </Button>
+  );
+};
+export default PrimaryButton;
--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,20 +3,27 @@ import Image from "next/image";
 import { FC } from "react";
 import HeroStyles from "./Hero.module.scss";
 import Subtitle1 from "../Typography/Subtitle1";
+import PrimaryButton from "../Buttons/PrimaryButton/PrimaryButton";
 
 const Hero: FC = () => {
   return (
     <div className={HeroStyles.Hero}>
-      <Box display={"flex"} flexDirection={"column"} flex={3}>
+      <div className={HeroStyles.LeftContent}>
         <Subtitle1>Insights</Subtitle1>
-        <Typography variant="h1">Personal Website</Typography>
+        <Typography variant="h1" fontWeight={600} fontSize={"3.75em"}>
+          Personal Website
+        </Typography>
         <Typography>
           Elit vis eleifend integer mi odio posuere duis mattis vestibulum
           penatibus libero mi felis massa vivamus porta viverra. Magna etiam
           lorem
         </Typography>
         {/* CTA */}
-      </Box>
+
+        <PrimaryButton className={HeroStyles.CTA}>
+          Magna etiam lorem
+        </PrimaryButton>
+      </div>
       <Box flex={2}>
         <div className={HeroStyles.ImageContainer}>
           <Image
