c367ec3b7187813b7802bdbe52e6312fc7357192
Ramon Polidura <ramonpoli@users.noreply.github.com>
2024-08-19 18:35:06 +0100

    Rename images to upload new images

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -24,14 +24,14 @@ export default function About() {
           {
             text: "Other hobbies of mine include playing video games, watching movies and series, playing board games, and reading books and graphic novels. I collect videogames, DBZ cards, graphic novels and board games.",
             image: {
-              url: "/with mahou.jpg",
+              url: "/with_mahou.jpg",
               alt: "Ramon Polidura drinking his favourite beer: Mahou",
             },
           },
           {
             text: "My favorite videogames are: Final Fantasy VII (the OG), Days gone, Starcraft, Satisfactory, The Binding of Isaac, Steampunk, and Oxygen Not Included. My favorite board games are: Arkham horror the card game, Aliens another glorious day in the corps, Splendor, and the OG chess.",
             image: {
-              url: "/chair playing.jpg",
+              url: "/chair_playing.jpg",
               alt: "Ramon Polidura pretending to play videogames on a chair",
             },
           },
