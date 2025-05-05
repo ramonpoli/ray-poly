c5f1647aac6ef77b13fc496ebb1aa72f6784eb5b
ramonpoli <ramon.polidura@gmail.com>
2025-05-05 09:13:10 +0100

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,39 +1,42 @@
-import { fetchBlogPosts } from '@/api/fetchBlogPosts';
-import { MetadataRoute } from 'next'
+import { fetchBlogPosts } from "@/api/fetchBlogPosts";
+import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-
   const blogPosts = fetchBlogPosts();
   return [
     {
       url: `${process.env.SERVER_URL}`,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 1,
     },
     {
       url: `${process.env.SERVER_URL}/about`,
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: "monthly",
       priority: 0.8,
     },
     {
       url: `${process.env.SERVER_URL}/services`,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.5,
     },
     {
       url: `${process.env.SERVER_URL}/services/e-commerce-website`,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.8,
     },
-    ...blogPosts?.map((blogPost) => ({
+    ...(blogPosts?.map((blogPost) => ({
       url: `${process.env.SERVER_URL}/blog/${blogPost.slug}`,
       lastModified: new Date(),
-    })) ?? [],
+    })) ?? []),
+    {
+      url: `${process.env.SERVER_URL}/__forms.html`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.8,
+    },
   ];
 }
-
-
