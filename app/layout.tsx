3f321a22b7da4b09b80363874261dab20edf8720
ramonpoli <ramon.polidura@gmail.com>
2025-05-05 08:00:59 +0100

    add simple form

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,58 +12,58 @@ import HomepageStyles from "../styles/homepage.module.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Ray Poly's website",
-  description:
-    "Ramon Polidura is a web developer, specialized in front-end development.",
-  applicationName: "ray-poly-personal-website",
-  referrer: "origin-when-cross-origin",
-  keywords: [
-    "Ray Poly",
-    "Ramon Polidura",
-    "Polidura Rueda",
-    "Web developer",
-    "Website development",
-    "London",
-  ],
-  authors: [
-    { name: "Ray Poly", url: "https://raypoly.netlify.app" },
-    { name: "Ramon Polidura", url: "https://raypoly.netlify.app" },
-  ],
-  creator: "Ray Poly",
-  publisher: "Ray Poly",
-  openGraph: {
-    title: "Ramon Polidura",
-    description:
-      "Ramon Polidura is a web developer, specialized in front-end development.",
-    type: "website",
-    image: "img/logos/logo.png",
-    site_name: "Ray Poly's website",
-  },
-  formatDetection: {
-    email: false,
-    address: false,
-    telephone: false,
-  },
+	title: "Ray Poly's website",
+	description:
+		"Ramon Polidura is a web developer, specialized in front-end development.",
+	applicationName: "ray-poly-personal-website",
+	referrer: "origin-when-cross-origin",
+	keywords: [
+		"Ray Poly",
+		"Ramon Polidura",
+		"Polidura Rueda",
+		"Web developer",
+		"Website development",
+		"London",
+	],
+	authors: [
+		{ name: "Ray Poly", url: "https://raypoly.netlify.app" },
+		{ name: "Ramon Polidura", url: "https://raypoly.netlify.app" },
+	],
+	creator: "Ray Poly",
+	publisher: "Ray Poly",
+	openGraph: {
+		title: "Ramon Polidura",
+		description:
+			"Ramon Polidura is a web developer, specialized in front-end development.",
+		type: "website",
+		image: "img/logos/logo.png",
+		site_name: "Ray Poly's website",
+	},
+	formatDetection: {
+		email: false,
+		address: false,
+		telephone: false,
+	},
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <StrictMode>
-      <ThemeProvider theme={theme}>
-        <html lang="en">
-          <GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID || ""} />
-          <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ""} />
-          <body className={inter.className}>
-            <Header />
-            <main className={HomepageStyles.Main}>{children}</main>
-            <Footer />
-          </body>
-        </html>
-      </ThemeProvider>
-    </StrictMode>
-  );
+	return (
+		<StrictMode>
+			<ThemeProvider theme={theme}>
+				<html lang="en">
+					<GoogleTagManager gtmId={process.env.GOOGLE_TAG_MANAGER_ID || ""} />
+					<GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ""} />
+					<body className={inter.className}>
+						<Header />
+						<main className={HomepageStyles.Main}>{children}</main>
+						<Footer />
+					</body>
+				</html>
+			</ThemeProvider>
+		</StrictMode>
+	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import Divider from "@/components/Divider/Divider";
 import Hero from "../components/Hero/Hero";
 import FeatureTiles from "@/components/FeatureTiles/FeatureTiles";
@@ -8,28 +8,61 @@ import { Fragment } from "react";
 import { sendGoogleEvent } from "@/utils/googleAnalytics";
 
 export default function Home() {
-  return (
-    <Fragment>
-      <Hero title={'Ramon Polidura Rueda'}
-        subtitle={'Senior web developer delivering excellent user experience since 2011. Utilizing the latest technologies and applying the best coding practices among web devs.'}
-        cta={{
-          onClick: () => {
-            sendGoogleEvent('moreAboutMeClick')
-          },
-          label: 'More about me'
-        }}
-        image={{
-          src: '/me.JPG',
-          alt: 'Ray Poly\'s profile image'
-        }}
-      />
-      < Divider />
-      <FeatureTiles />
-      <Divider />
-      <SocialMedias />
-      <Divider />
-      <ListOfFeatures />
-      <Divider />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Hero
+				title={"Ramon Polidura Rueda"}
+				subtitle={
+					"Senior web developer delivering excellent user experience since 2011. Utilizing the latest technologies and applying the best coding practices among web devs."
+				}
+				cta={{
+					onClick: () => {
+						sendGoogleEvent("moreAboutMeClick");
+					},
+					label: "More about me",
+				}}
+				image={{
+					src: "/me.JPG",
+					alt: "Ray Poly's profile image",
+				}}
+			/>
+			<Divider />
+			<form name="contact" method="POST" data-netlify="true">
+				<p>
+					<label>
+						Your Name: <input type="text" name="name" />
+					</label>
+				</p>
+				<p>
+					<label>
+						Your Email: <input type="email" name="email" />
+					</label>
+				</p>
+				<p>
+					<label>
+						Your Role:{" "}
+						<select name="role[]" multiple>
+							<option value="leader">Leader</option>
+							<option value="follower">Follower</option>
+						</select>
+					</label>
+				</p>
+				<p>
+					<label>
+						Message: <textarea name="message"></textarea>
+					</label>
+				</p>
+				<p>
+					<button type="submit">Send</button>
+				</p>
+			</form>
+			<Divider />
+			<FeatureTiles />
+			<Divider />
+			<SocialMedias />
+			<Divider />
+			<ListOfFeatures />
+			<Divider />
+		</Fragment>
+	);
 }
