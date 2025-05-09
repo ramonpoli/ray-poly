d24ace917a6e91e2adec802e9d8590534417388d
ramonpoli <ramon.polidura@gmail.com>
2025-05-09 12:33:26 +0100

    Feature list

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,11 +1,11 @@
 "use client";
 import {
-  AppBar,
-  IconButton,
-  Menu,
-  MenuItem,
-  Toolbar,
-  Typography,
+	AppBar,
+	IconButton,
+	Menu,
+	MenuItem,
+	Toolbar,
+	Typography,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { FC, useState } from "react";
@@ -15,75 +15,90 @@ import { sendGoogleEvent } from "@/utils/googleAnalytics";
 import Image from "next/image";
 
 const MenuItems: { link: string; text: string }[] = [
-  { link: "/about", text: "About me" },
-  { link: "/blog", text: "Blog" },
-  { link: "/services", text: "Services" },
+	{ link: "/about", text: "About me" },
+	{ link: "/blog", text: "Blog" },
+	{ link: "/services", text: "Services" },
 ];
 
 const Header: FC = () => {
-  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+	const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
+	const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorElNav(event.currentTarget);
+	};
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
 
-  return (
-    <AppBar
-      position="static"
-      component="nav"
-      sx={{ zIndex: 2000, backgroundColor: "none" }}
-    >
-      <Toolbar sx={{ backgroundColor: "background.paper" }}>
-        <IconButton
-          size="large"
-          edge="start"
-          color="primary"
-          aria-label="menu"
-          className={"hide-desktop"}
-          sx={{ mr: 2 }}
-          onClick={handleOpenNavMenu}
-        >
-          <MenuIcon />
-        </IconButton>
+	return (
+		<AppBar
+			position="fixed"
+			component="nav"
+			sx={{ zIndex: 2000, backgroundColor: "none" }}
+		>
+			<Toolbar sx={{ backgroundColor: "background.paper" }}>
+				<IconButton
+					size="large"
+					edge="start"
+					color="primary"
+					aria-label="menu"
+					className={"hide-desktop"}
+					sx={{ mr: 2 }}
+					onClick={handleOpenNavMenu}
+				>
+					<MenuIcon />
+				</IconButton>
 
-        <Menu
-          className={styles.Nav}
-          anchorEl={anchorElNav}
-          anchorOrigin={{
-            vertical: "bottom",
-            horizontal: "left",
-          }}
-          open={Boolean(anchorElNav)}
-          transformOrigin={{
-            vertical: "top",
-            horizontal: "left",
-          }}
-          onClose={handleCloseNavMenu}
-        >
-          {MenuItems.map((item) => (
-            <MenuItem className={styles.NavItem} key={item.text}>
-              <Link href={item.link} onClick={() => { sendGoogleEvent(`navbar_${item.text}Clicked`) }}>
-                <Typography color={"text.primary"}>{item.text}</Typography>
-              </Link>
-            </MenuItem>
-          ))}
-        </Menu>
-        <Link href={"/"} onClick={() => { sendGoogleEvent('navbar_logoClicked') }}>
-          <Image src={'/logos/logo-landscape.png'} alt={'Ray Poly\'s logo'} width={150} height={50} />
-        </Link>
-        <nav className={`${styles.Nav} ${styles.NavDesktop}`}>
-          {MenuItems.map((item) => (
-            <Link href={item.link} key={item.text} className={styles.NavItem}>
-              <Typography color={"text.primary"}>{item.text}</Typography>
-            </Link>
-          ))}
-        </nav>
-      </Toolbar>
-    </AppBar>
-  );
+				<Menu
+					className={styles.Nav}
+					anchorEl={anchorElNav}
+					anchorOrigin={{
+						vertical: "bottom",
+						horizontal: "left",
+					}}
+					open={Boolean(anchorElNav)}
+					transformOrigin={{
+						vertical: "top",
+						horizontal: "left",
+					}}
+					onClose={handleCloseNavMenu}
+				>
+					{MenuItems.map((item) => (
+						<MenuItem className={styles.NavItem} key={item.text}>
+							<Link
+								href={item.link}
+								onClick={() => {
+									sendGoogleEvent(`navbar_${item.text}Clicked`);
+								}}
+							>
+								<Typography color={"text.primary"}>{item.text}</Typography>
+							</Link>
+						</MenuItem>
+					))}
+				</Menu>
+				<Link
+					href={"/"}
+					onClick={() => {
+						sendGoogleEvent("navbar_logoClicked");
+					}}
+				>
+					<Image
+						src={"/logos/logo-landscape.png"}
+						alt={"Ray Poly's logo"}
+						width={150}
+						height={50}
+					/>
+				</Link>
+				<nav className={`${styles.Nav} ${styles.NavDesktop}`}>
+					{MenuItems.map((item) => (
+						<Link href={item.link} key={item.text} className={styles.NavItem}>
+							<Typography color={"text.primary"}>{item.text}</Typography>
+						</Link>
+					))}
+				</nav>
+			</Toolbar>
+		</AppBar>
+	);
 };
 export default Header;
--- components/ListOfFeatures/ListOfFeatures.tsx
+++ components/ListOfFeatures/ListOfFeatures.tsx
@@ -1,64 +1,70 @@
-'use client'
+"use client";
 import React from "react";
 import styles from "./ListOfFeatures.module.scss";
 import { Card, CardContent, Typography } from "@mui/material";
 import { sendGoogleEvent } from "@/utils/googleAnalytics";
 
 interface Skill {
-  name: string;
-  description: string;
+	name: string;
+	description: string;
 }
 
 const skills: Skill[] = [
-  {
-    name: "React",
-    description: "A JavaScript library for building user interfaces",
-  },
-  {
-    name: "TypeScript",
-    description:
-      "A typed superset of JavaScript that compiles to plain JavaScript",
-  },
-  {
-    name: "Node.js",
-    description:
-      "A JavaScript runtime built on Chrome's V8 JavaScript engine for server-side development",
-  },
-  {
-    name: "Next.js",
-    description: "A React framework for production-grade applications",
-  },
-  {
-    name: "HTML5/CSS3",
-    description:
-      "Modern web technologies for structuring content and styling web pages",
-  },
+	{
+		name: "React",
+		description: "A JavaScript library for building user interfaces",
+	},
+	{
+		name: "TypeScript",
+		description:
+			"A typed superset of JavaScript that compiles to plain JavaScript",
+	},
+	{
+		name: "Node.js",
+		description:
+			"A JavaScript runtime built on Chrome's V8 JavaScript engine for server-side development",
+	},
+	{
+		name: "Next.js",
+		description: "A React framework for production-grade applications",
+	},
+	{
+		name: "HTML5/CSS3",
+		description:
+			"Modern web technologies for structuring content and styling web pages",
+	},
 ];
 
 const ListOfFeatures: React.FC = () => {
-  return (
-    <section className={styles.container}>
-      <Typography variant="h2" className={styles.title}>
-        Core Skills
-      </Typography>
-      <ul className={styles.list}>
-        {skills.map((skill, index) => (
-          <li key={index} className={styles.item} onMouseEnter={() => { sendGoogleEvent('hoverOverSkill', skill.name) }}>
-            <Card className={styles.card}>
-              <CardContent className={styles.cardContent}>
-                <Typography variant="h4" className={styles.skillName}>
-                  {skill.name}
-                </Typography>
-                <Typography className={styles.skillDescription}>
-                  {skill.description}
-                </Typography>
-              </CardContent>
-            </Card>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
+	return (
+		<section className={styles.container}>
+			<Typography variant="h2" className={styles.title}>
+				Core Skills
+			</Typography>
+			<ul className={styles.list}>
+				{skills.map((skill, index) => (
+					<li
+						key={`${skill.name}-${index}`}
+						className={styles.item}
+						onMouseEnter={() => {
+							sendGoogleEvent("hoverOverSkill", skill.name);
+						}}
+					>
+						<Card className={styles.card}>
+							<CardContent className={styles.cardContent}>
+								<Typography variant="h4" className={styles.skillName}>
+									{skill.name}
+								</Typography>
+								<Typography className={styles.skillDescription}>
+									{skill.description}
+								</Typography>
+							</CardContent>
+						</Card>
+					</li>
+				))}
+			</ul>
+		</section>
+	);
 };
 
 export default ListOfFeatures;
